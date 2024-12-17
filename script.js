e7232f23ed3674efd05ad08d17e0304a054dff1a
Lalit Mohan <111294030+Lalit-mp@users.noreply.github.com>
2024-12-17 16:41:36 +0530

    Update script.js
    Now all buttons are working

--- script.js
+++ script.js
@@ -2,46 +2,51 @@ const allButton = document.querySelector(".button-menu");
 const homePageCtn = document.querySelector(".home");
 const profilePageCtn = document.querySelector(".profile");
 const contactPageCtn = document.querySelector(".contact");
-allButton.addEventListener("click",(e)=>{
-    // console.log(e.target.textContent);
-    const homePage= allButton.children[0];
-    const profilePage= allButton.children[1];
-    const contactPage= allButton.children[2];
-    if(e.target.textContent === "Home"){
-        profilePageCtn.classList.add("hide");
-        contactPageCtn.classList.add("hide");
 
-        homePage.classList.add("button-bg")
-        profilePage.classList.remove("button-bg")
-        contactPage.classList.remove("button-bg")
-        console.log("i am home")
-    } else if(e.target.textContent === "Profile"){
-        homePageCtn.classList.add("hide");
-        profilePageCtn.classList.add("show");
-        // contactPageCtn.classList.add("show");
+profilePageCtn.classList.add("hide");
+contactPageCtn.classList.add("hide");
+console.log(homePageCtn);
+console.log(profilePageCtn);
+console.log(contactPageCtn);
 
-        profilePage.classList.add("button-bg")
-        homePage.classList.remove("button-bg")
-        contactPage.classList.remove("button-bg")
-        console.log("i am Profile")
-    } else if(e.target.textContent === "Contact"){
-        homePageCtn.classList.add("hide");
-        profilePageCtn.classList.add("hide");
-        contactPageCtn.classList.add("show");
-
-        const contactPage= allButton.children[2];
-        profilePage.classList.remove("button-bg")
-        homePage.classList.remove("button-bg")
-        contactPage.classList.add("button-bg")
-        console.log("i am Contact")
-    } else{
-        profilePageCtn.classList.add("hide");
-        contactPageCtn.classList.add("hide");
-
-        homePage.classList.add("button-bg")
-        profilePage.classList.remove("button-bg")
-        contactPage.classList.remove("button-bg")
-        console.log("i am home")
+allButton.addEventListener("click", (e) => {
+  console.log(e.target.textContent);
+  if (e.target.textContent === "Home") {
+    allButton.children[0].classList.add("button-bg");
+    if (
+      allButton.children[1].classList.contains("button-bg") ||
+      allButton.children[2].classList.contains("button-bg")
+    ) {
+      allButton.children[1].classList.remove("button-bg");
+      allButton.children[2].classList.remove("button-bg");
     }
-})
-// console.log(homeButton);
+    profilePageCtn.classList.add("hide");
+        homePageCtn.classList.remove("hide");
+        contactPageCtn.classList.add("hide")
+
+  } else if (e.target.textContent === "Profile") {
+    allButton.children[1].classList.add("button-bg");
+    if (
+      allButton.children[0].classList.contains("button-bg") ||
+      allButton.children[2].classList.contains("button-bg")
+    ) {
+      allButton.children[0].classList.remove("button-bg");
+      allButton.children[2].classList.remove("button-bg");
+    }
+    profilePageCtn.classList.remove("hide");
+        homePageCtn.classList.add("hide");
+        contactPageCtn.classList.add("hide");
+  } else if (e.target.textContent === "Contact") {
+    allButton.children[2].classList.add("button-bg");
+    if (
+      allButton.children[1].classList.contains("button-bg") ||
+      allButton.children[0].classList.contains("button-bg")
+    ) {
+      allButton.children[1].classList.remove("button-bg");
+      allButton.children[0].classList.remove("button-bg");
+    }
+    contactPageCtn.classList.remove("hide");
+        profilePageCtn.classList.add("hide");
+        homePageCtn.classList.add("hide");
+  }
+});
